3439a5b006101479ef815428098fc5c85e669c7c
cji <gugugeul273@gmail.com>
2025-11-04 00:53:38 +0900

    feat(all):SSG 방식으로 변경

--- src/components/book-item.tsx
+++ src/components/book-item.tsx
@@ -1,18 +1,19 @@
 import { BookData } from "@/types";
 import Link from "next/link";
 import style from "@/components/book-item.module.css"
+import Image from "next/image";
 export default function BookItem({
     id,
     title,
     subTitle,
-    description,
+    // description,
     author,
     publisher,
     coverImgUrl
 }:BookData){
     return <Link href={`/book/${id}`} className={style.container}>
         
-        <img src={coverImgUrl}></img>
+        <Image src={coverImgUrl} alt="이미지"/>
         <div><div  className={style.title}>{title}</div>
         <div  className={style.subTitle}>{subTitle}</div>
         <br/>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,24 +1,40 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter} from "next/router"
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState, useCallback } from "react";
 import BookItem from "@/components/book-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
+import { useRouter } from "next/router";
+import { BookData } from "@/types";
 
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-console.log("콘텍스트",context)
-const q = context.query.q;
-const books = await fetchBooks(q as string);
+// export const getStaticProps = async (context: GetStaticPropsContext) => {
+// console.log("콘텍스트",context)
+// const q = context.query.q;
+// const books = await fetchBooks(q as string);
 
-return{
+// return{
     
-    props:{
-        books,
-    },
-};
-}
-export default function Page({books}:InferGetServerSidePropsType<typeof getServerSideProps>){
+//     props:{
+//         books,
+//     },
+// };
+// }
+export default function Page(){
+    const[books, setBooks] = useState<BookData[]>([]);
+    
+    const router = useRouter();
+    const q = router.query.q;
+
+    const fetchSearchResult =  useCallback(async() =>{
+        const data = await fetchBooks(q as string);
+        setBooks(data);
+    },[q])
+
+    useEffect(() => {
+        if(q){
+            fetchSearchResult();
+        }
+    },[fetchSearchResult]);
+
    return(
    <div>
     {books.map((book)=>(<BookItem key={book.id} {...book} />
